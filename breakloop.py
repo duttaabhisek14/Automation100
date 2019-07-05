739d9ec0c4eff72f40b3326d1bb35bb01c8221ba
duttaabhisek14 <duttaabhisek14@gmail.com>
2019-07-06 01:54:21 +0530

    automation files

---
+++ breakloop.py
@@ -0,0 +1,23 @@
+x=0
+while x<10:
+    print ("print the num"+str(x))
+    x=x+1
+   # if x==2:
+        #print("print the no"+str(x))
+       # break
+
+       # print("*******************")
+
+else:
+
+    print("print this statement")
+
+
+
+
+
+
+
+
+
+
